f621b4563db464a69a99cd4c9d4a8bd1075cb7bf
S1mpl3ton <titanikpotonyl@gmail.com>
2022-11-10 23:32:12 +0400

    skrept

--- skript.cs
+++ skript.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class skript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int score; 
+    public Text clicktext;
+
     void Start()
     {
-        
+        score = 0;
+        score = playerPrefs.GetInt("score", score);
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        clicktext.text = score.ToString();
+    }
+    public void ClickerScore()
+    {
+        score++;
+        playerPrefs.SetInt("score+", score);
+    }
+    public void Reset()
+    {
+        playerPrefs.deleteAll();
     }
 }
