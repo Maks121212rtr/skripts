032d2600a5609f26e55a997f63dfc9503d409f36
kap <maxhawkyt@gmail.com>
2022-11-13 00:30:52 +0400

    commit

--- skript.cs
+++ skript.cs
@@ -2,18 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class skript : MonoBehaviour
 {
     [SerializeField] int money; 
-    public int total_money;
     public Text moneyText;
 
     void Start()
     {
         money = PlayerPrefs.GetInt("money", money);
-        total_money = PlayerPrefs.GetInt("total_money", total_money);
     }
 
    
@@ -25,14 +22,7 @@ public class skript : MonoBehaviour
     public void Clickermoney()
     {
         money++;
-        total_money++;
         PlayerPrefs.SetInt("money", money);
-        PlayerPrefs.SetInt("total_money", total_money);
-    }
-
-    public void ToDos()
-    {
-        SceneManager.LoadScene(1);
     }
 
     public void Reset()
